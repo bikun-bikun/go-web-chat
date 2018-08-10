2c5d7f014a9c2998797f146a3fa9013d1cb03276
bikun-bikun <big.stone.net@gmail.com>
2018-08-10 17:35:08 +0900

    mod room

--- room.go
+++ room.go
@@ -1,5 +1,8 @@
 package main
 
 type room struct {
-	forward chan []byte
+	forward chan []byte      // 他のクライアントにメッセージを保存するためのチャネル
+	join    chan *client     // チャットルームに参加しようとしているクライアントのためのチャネル
+	leave   chan *client     // チャットルームから体質仕様としているクライアントのためのチャネル
+	clients map[*client]bool // 在室しているすべてのクライアントが保持される
 }
