45f177f81fc87a59d840851e3b7bc4f4a30aba73
bikun-bikun <big.stone.net@gmail.com>
2018-11-09 21:20:21 +0900

    add tracer package

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
+	"trace"
 )
 
 type templateHandler struct {
@@ -29,6 +31,7 @@ func main() {
 	flag.Parse()
 
 	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 
--- room.go
+++ room.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"trace"
 )
 
 type room struct {
@@ -11,6 +12,7 @@ type room struct {
 	join    chan *client     // チャットルームに参加しようとしているクライアントのためのチャネル
 	leave   chan *client     // チャットルームから体質仕様としているクライアントのためのチャネル
 	clients map[*client]bool // 在室しているすべてのクライアントが保持される
+	tracer  trace.Tracer     //tracerはチャットルームで行われた操作ログを受け取ります。
 }
 
 func newRoom() *room {
@@ -27,17 +29,21 @@ func (r *room) run() {
 		select {
 		case client := <-r.join:
 			r.clients[client] = true
+			r.tracer.Trace("新しいクライアントが参加しました")
 		case client := <-r.leave:
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("クライアントが退室しました")
 		case msg := <-r.forward:
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
 					//メッセージを送信
+					r.tracer.Trace("-- クライアントに送信されました")
 				default:
 					delete(r.clients, client)
 					close(client.send)
+					r.tracer.Trace("-- 送信に失敗しました。クライアントをクリーンアップします")
 				}
 			}
 		}
