556a2eacfd685d0d45aeaa27fb4277b08800097e
bikun-bikun <big.stone.net@gmail.com>
2018-11-22 01:50:31 +0900

    add config and read yaml

--- main.go
+++ main.go
@@ -2,11 +2,37 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"github.com/stretchr/gomniauth"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"path/filepath"
 	"sync"
+
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type config struct {
+	Google   oauthConfig `yaml:"google"`
+	GitHub   oauthConfig `yaml:"github"`
+	Facebook oauthConfig `yaml:"facebook"`
+}
+
+type oauthConfig struct {
+	ClientId    string `yaml:"clientId"`
+	Secret      string `yaml:"secret"`
+	RedirectUri string `yaml:"redirectUri"`
+}
+
+var (
+	configrationPath = "env.yml"
+	conf             config
 )
 
 type templateHandler struct {
@@ -25,12 +51,33 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	buf, err := ioutil.ReadFile(configrationPath)
+	if err != nil {
+		return
+	}
+
+	err = yaml.Unmarshal(buf, &conf)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(conf.Google.ClientId)
+
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
+
+	gomniauth.SetSecurityKey("hogehoge1234")
+	gomniauth.WithProviders(
+		facebook.New("", "", ""),
+		github.New("", "", ""),
+		google.New(conf.Google.ClientId, conf.Google.Secret, conf.Google.RedirectUri),
+	)
 
 	r := newRoom()
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	go r.run()
