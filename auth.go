b4ef3a1b023cbff3f60bd2e14c2b07f270613660
bikun-bikun <big.stone.net@gmail.com>
2018-12-19 01:49:53 +0900

    add auth.go struct

--- auth.go
+++ auth.go
@@ -10,7 +10,22 @@ import (
 	"strings"
 
 	"github.com/stretchr/gomniauth"
+	gomniauthcommon "github.com/stretchr/gomniauth/common"
 )
+
+type ChatUser interface {
+	UniqueID() string
+	AvatarURL() string
+}
+
+type chatUser struct {
+	gomniauthcommon.User
+	uniqueID string
+}
+
+func (u chatUser) UniqueID() string {
+	return u.uniqueID
+}
 
 type authHandler struct {
 	next http.Handler
